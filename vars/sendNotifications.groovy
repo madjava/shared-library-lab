3775b66bf931f5171a27d55bf97bbe9afce47c65
felix.eyetan <felix.eyetan@justice.gov.uk>
2024-04-07 16:47:32 +0100

    update email details

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -3,7 +3,6 @@ def call(Map config = [:]) {
 emailext (
   subject: "${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
   mimetype: 'text/html',
-  to: 'admin@mailhog.local',
   body: """<p>${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p><p>Repo: ${config.repo}</p>
            <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
   recipientProviders: [[$class: 'RequesterRecipientProvider']]
